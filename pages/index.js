178603fcc5432fbcf27157e4aee2a64eb1b5ccf0
Luizsepa <luizaugustocavalheri7@outlook.com>
2025-05-21 02:56:44 +0000

    Mudando dnv so pra testar

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,10 @@
 function Home(){
-    return <h1>Obrigado por me acompanhar!! 🙏</h1>
+    return <h1>
+        Estou adorando o Curso.dev e tenho que agradecer
+        a minha mae Michele, por dar pra mim essa oportunidade
+        de estar participando deste curso!!!!!
+        Obrigado por me acompanhar!! 🙏
+    </h1>
 }
 
-export default Home; 
+export default Home; //testando no Vscode normal
