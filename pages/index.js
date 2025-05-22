9a1a13752b740b36a85599cb3caa4a3a7d835e62
Luizsepa <Luizaugustocavalheri7@outlook.com>
2025-05-21 23:49:03 -0300

    adicina scrips ´lint:cheak´ e ´lint:fix´

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
-function Home(){
-    return <h1>
-        Estou adorando o Curso.dev e tenho que agradecer
-        a minha mae Michele, por dar pra mim essa oportunidade
-        de estar participando deste curso!!!!!
-        Obrigado por me acompanhar!! 🙏
+import { captureOwnerStack } from "react";
+
+function Home() {
+  return (
+    <h1>
+      Estou adorando o Curso.dev e tenho que agradecer a minha mae Michele, por
+      dar pra mim essa oportunidade de estar participando deste curso!!!!!
+      Obrigado por me acompanhar!! 🙏
     </h1>
+  );
 }
 
 export default Home; //testando no Vscode normal
